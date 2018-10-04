8391b02714070eda41140208dd045e63be779929
GodefroiRoussel <godefroi.roussel@gmail.com>
2018-10-04 19:45:39 +0200

    Refactor code

--- src/main/scala/Helper.scala
+++ src/main/scala/Helper.scala
@@ -2,6 +2,71 @@ import scala.io.StdIn
 
 object Helper {
 
+    /**
+      * Function asking to the user to enter a number to choose which mode to play and check if the user entered a correct parameter
+      * "1" => To play the game either against a human or an AI
+      * "2" => To test AI and make them fight together
+      * Other => Will quit the program
+      * @return the input of the user
+      */
+    def chooseMode(): String = {
+        println ("Do you want to play to the battleship or do you want to see AI fights ?\n" +
+            "Press 1 to play the game.\n" +
+            "Press 2 to see AI fights.\n" +
+            "Press any other key to quit the program.")
+
+        StdIn.readLine()
+    }
+
+    /**
+      * Function asking to the user to enter a number to choose which game to play and check if the user entered a correct parameter
+      * "1" => To play the game against the human
+      * "2" => To play the game against an AI
+      * Other => call again the chooseOpponent function untill the user enter a 1 or 2
+      * @return the input of the user
+      */
+    def chooseOpponent(): String = {
+        println ("Do you want to play against a human or against an AI?\n" +
+            "Press 1 to play against a human.\n" +
+            "Press 2 to play against an AI.\n")
+
+        val opponent: String = StdIn.readLine()
+        opponent match {
+            case "1" | "2" =>
+                // Play against a Human or an AI
+                opponent
+            case _ =>
+                // Choose an Opponent again
+                println("You choosed a wrong parameter. Please try again.")
+                chooseOpponent()
+        }
+    }
+
+    /**
+      * Function asking to the user to enter a number to choose the level of the AI and check if the user entered a correct parameter
+      * "1" => To play against the easiest AI
+      * "2" => To play the game against the medium AI
+      * "3" => To play against the hardest AI
+      * Other => Call again the chooseLevel function untill the user enter a 1, 2 or 3
+      * @return the input of the user
+      */
+    def chooseLevel(): String = {
+        println ("Which AI do you want to play against?\n" +
+            "Press 1 to play against an easy AI.\n" +
+            "Press 2 to play against a medium AI.\n" +
+            "Press 3 to play against a hard AI.\n")
+
+        val level = StdIn.readLine()
+        level match {
+            case "1" | "2" | "3" =>
+                // Play against a Human or an AI
+                level
+            case _ =>
+                // Choose an Opponent again
+                println("You choosed a wrong parameter. Please try again.")
+                chooseLevel()
+        }
+    }
 
     /**
       * Function asking to the user to enter a letter to choose which column to shoot and check if the user entered a correct parameter
